f144b80af2bcaeb4142971de8e9018bf38948d1f
Saeed Mohagheghi <saeed.discovery@gmail.com>
2023-01-23 02:13:46 +0330

    show spinner of running function again!

--- 🤖_Doc._Chat.py
+++ 🤖_Doc._Chat.py
@@ -41,7 +41,7 @@ def new_question():
     pass
 
 
-@st.cache(suppress_st_warning=True, show_spinner=False)
+@st.cache(suppress_st_warning=True)
 def show_answer():
     with st.spinner('Please wait...'):
         headers = {'Content-Type': 'application/json; charset=utf-8'}
