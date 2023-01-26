b0f083322a8f3dbd6c5e893b46754f3e47ba2e0e
Saeed Mohagheghi <saeed.discovery@gmail.com>
2023-01-26 09:49:42 +0330

    changed os.getenv to os.environ

--- 🤖_Doc_Chat.py
+++ 🤖_Doc_Chat.py
@@ -25,7 +25,8 @@ COMPLETIONS_API_PARAMS = {
     "model": COMPLETIONS_MODEL,
 }
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ("OPENAI_API_KEY")
 header = """Answer the question as truthfully as possible using the provided context, \
 and include the parts of the context that are used to generate the answer after the answer starting with "\nRef:", \
 and if the answer is not contained within the text below, say "I don't know."\n\nContext:\n"""
